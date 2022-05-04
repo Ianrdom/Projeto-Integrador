95b7d8a5495e629de4c6ab2f90e3261bd1c25c09
Ian-2INFO3 <ianrobertoestudos10@gmail.com>
2022-05-04 19:47:24 -0300

    feito

--- Codigo-Fonte/src/components/BlocoCabecalho.vue
+++ Codigo-Fonte/src/components/BlocoCabecalho.vue
@@ -1,5 +1,12 @@
 <template>
   <header id="header">
     <img src="src/assets/ifc-logo.png" alt="algo" />
+
+    <div class="header-right">
+      <ul class="menu-header">
+        <li class="items-menu">Usuario</li>
+        <li class="items-menu">Menu</li>
+      </ul>
+    </div>
   </header>
 </template>
--- Codigo-Fonte/src/components/BlocoRodape.vue
+++ Codigo-Fonte/src/components/BlocoRodape.vue
@@ -1,3 +1,10 @@
 <template>
-  <footer id="footer"></footer>
+  <footer id="footer">
+    <div class="footer">
+      <ul class="menu-footer">
+        <li class="items-menu">Email: ifcemailgenerico@ifc.edu.br</li>
+        <li class="items-menu">Telefone: 47 9876-5432</li>
+      </ul>
+    </div>
+  </footer>
 </template>
