62af0bff8f389c9c3d9a5394a1a5497e3d5ce7e7
Ian-2INFO3 <ianrobertoestudos10@gmail.com>
2022-05-07 13:31:44 -0300

    a

--- Codigo-Fonte/src/components/BlocoRodape.vue
+++ Codigo-Fonte/src/components/BlocoRodape.vue
@@ -5,7 +5,7 @@
         <li class="items-menu items-footer">
           Email: ifcemailgenerico@ifc.edu.br
         </li>
-        <li class="items-menu items-footer">Telefone: 47 9876-5432</li>
+        <li class="items-menu items-footer">Telefone: 47 9876-4321</li>
       </ul>
     </div>
   </footer>
