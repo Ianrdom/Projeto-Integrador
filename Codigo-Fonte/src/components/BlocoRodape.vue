ce67b1df0bbc27669538fe80fadf1f5bac1d47c5
Ian-2INFO3 <ianrobertoestudos10@gmail.com>
2022-05-09 21:29:14 -0300

    adicionando router

--- Codigo-Fonte/src/components/BlocoRodape.vue
+++ Codigo-Fonte/src/components/BlocoRodape.vue
@@ -2,10 +2,8 @@
   <footer id="footer">
     <div class="footer">
       <ul class="menu-footer">
-        <li class="items-menu items-footer">
-          Email: ifcemailgenerico@ifc.edu.br
-        </li>
-        <li class="items-menu items-footer">Telefone: 47 9876-4321</li>
+        <li class="items-menu items-footer">Contato</li>
+        <li class="items-menu items-footer">Sobre Nós</li>
       </ul>
     </div>
   </footer>
