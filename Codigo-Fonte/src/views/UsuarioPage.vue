1c580b97d69c1127450c0384406100d323d745fb
Ian-2INFO3 <ianrobertoestudos10@gmail.com>
2022-05-19 11:34:08 -0300

    pagina de usuario

--- Codigo-Fonte/src/views/UsuarioPage.vue
+++ Codigo-Fonte/src/views/UsuarioPage.vue
@@ -3,14 +3,16 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <main id="content" class="contato">
+  <main id="content" class="usuario">
     <h1>Usuario Autenticado</h1>
     <h2>Prefencias Alimentares</h2>
-    <ul>
-      <li>
-        <input type="checkbox" name="" id="" />
-      </li>
-    </ul>
+    <div class="preferencias">
+      <ul>
+        <li><input type="checkbox" /> Intolerante à lactose</li>
+        <li><input type="checkbox" /> Intolerante à glúten</li>
+        <li><input type="checkbox" /> Cardapio vegetariano</li>
+      </ul>
+    </div>
     <RouterLink to="/">Voltar</RouterLink>
   </main>
 </template>
